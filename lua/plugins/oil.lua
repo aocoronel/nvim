ce87ac12870cc7f3ba894479789ae988e39d503a
Augusto Coronel <aocoronel@proton.me>
2026-01-12 13:21:29 -0300

    chore(oil): remove mini.icons dep

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -128,7 +128,7 @@ end
 return {
     "stevearc/oil.nvim",
     opts = {},
-    dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+    -- dependencies = { { "nvim-mini/mini.icons", opts = {} } },
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
     lazy = false,
     config = config,
