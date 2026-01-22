514338879df570bdf263582858cb66ba78fd4b97
Augusto Coronel <aocoronel@proton.me>
2026-01-22 15:49:22 -0300

    chore(blink): change bindings, due to conflicts with insert bindings

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -6,12 +6,10 @@ return {
         keymap = {
             preset = 'none',
             ['<CR>'] = { 'accept', 'fallback' },
-            ['<Tab>'] = { 'snippet_forward', 'fallback' },
-            ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
-            ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
-            ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
-            ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
-            ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+            ['<M-p>'] = { 'select_prev', 'fallback_to_mappings' },
+            ['<M-n>'] = { 'select_next', 'fallback_to_mappings' },
+            ['<M-b>'] = { 'scroll_documentation_up', 'fallback' },
+            ['<M-f>'] = { 'scroll_documentation_down', 'fallback' },
         },
         appearance = {
             nerd_font_variant = "mono",
