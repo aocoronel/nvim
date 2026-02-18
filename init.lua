f969a265e4193965f8932376fdacd51c07323984
Augusto Coronel <aocoronel@proton.me>
2026-02-18 19:15:37 -0300

    chore: remove scroll animation from neovide

--- init.lua
+++ init.lua
@@ -2,5 +2,6 @@ require "config.init"
 
 if vim.g.neovide then
     vim.g.neovide_scale_factor = 0.9
+    vim.g.neovide_scroll_animation_length = 0.0
     vim.o.guifont = "JetBrainsMono Nerd Font Mono:h13"
 end
