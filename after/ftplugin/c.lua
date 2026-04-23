42465c31c4824602bb63d190e7c6a7254ae716f1
Augusto Coronel <aocoronel@proton.me>
2026-04-23 15:08:33 -0300

    reduce c indent

--- after/ftplugin/c.lua
+++ after/ftplugin/c.lua
@@ -1,6 +1,6 @@
-vim.opt.tabstop = 8
-vim.opt.shiftwidth = 8
-vim.opt.softtabstop = 8
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 vim.opt.wrap = false
 vim.opt.linebreak = true
